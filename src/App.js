73df69679aadaa3016017893a8e085f2e15f23cf
Ryan <thabet.abe@gmail.com>
2021-11-24 16:55:03 -0700

    added the main components and configured react Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Route, Switch} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
+import Navbar from './components/Navbar'
+import ProductList from './components/ProductList'
+import Details from './components/Details'
+import Cart from './components/Cart'
+import Default from './components/Default'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return (
+      <>
+        <Navbar/>
+        <Switch>
+          <Route path="/" exact><ProductList></ProductList></Route>
+          <Route path="/details"><Details/></Route>
+          <Route path="/cart"><Cart/></Route>
+          <Route> <Default/></Route>
+        </Switch>
+      </>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Default.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Default() {
+    return (
+        <div>
+            <h3>page not found</h3>
+        </div>
+    )
+}
